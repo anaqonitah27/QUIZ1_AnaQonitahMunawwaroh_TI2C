4e0439e731b0da19342ba80438153192644c4013
anaqonitah27 <anaqonitah04@gmail.com>
2022-03-25 16:06:16 +0700

    Membuat Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -31,4 +31,5 @@
     <h3>Socialize</h3>
     <img src="img/icon_fb.png" alt=""> <img src="img/icon_twitter.png" alt=""> <img src="img/icon_in.png" alt=""> </div>
   <div style="clear:both"></div>
+  
 </div>
